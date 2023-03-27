85c667e969348f23d9e6be790da23c474113a72c
Osman Melih Tolunay <78886346+Ilovejam@users.noreply.github.com>
2023-03-27 13:08:33 +0300

    new ui update

--- app.py
+++ app.py
@@ -3,19 +3,16 @@ import os
 import openai
 from time import time,sleep
 
-
-
 app = Flask(__name__)
 
-def open_file(filepath):
-    with open(filepath, 'r', encoding='utf-8') as infile:
-        return infile.read()
+from dotenv import load_dotenv
+load_dotenv()
 
-openai.api_key = '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'
-
-def bot(prompt, engine='text-davinci-003', temp=0.7, top_p=1, tokens=256, freq_pen=0, pres_pen=0, stop=['<<END>>']):
+def bot(prompt, engine='text-davinci-002', temp=0.9, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.5, stop=['<<END>>']):
     max_retry = 1
     retry = 0
+    api_key = os.getenv('api_key')  # Get API key from .env file
+    openai.api_key = api_key
     while True:
         try:
             response = openai.Completion.create(
@@ -29,15 +26,15 @@ def bot(prompt, engine='text-davinci-003', temp=0.7, top_p=1, tokens=256, freq_p
                 stop=[" User:", " AI:"])
             text = response['choices'][0]['text'].strip()
             print(text)
-            filename = '%s_alpha.txt' % time()
-            with open('logs/%s' % filename, 'w') as outfile:
+            filename = '%s_gpt3.txt' % time()
+            with open('gpt3_logs/%s' % filename, 'w') as outfile:
                outfile.write('PROMPT:\n\n' + prompt + '\n\n==========\n\nRESPONSE:\n\n' + text)
             return text
         except Exception as oops:
             retry += 1
             if retry >= max_retry:
-                return "alpha error: %s" % oops
-            print('Error communicating with alpha:', oops)
+                return "GPT3 error: %s" % oops
+            print('Error communicating with OpenAI:', oops)
             sleep(1)
 
 
@@ -52,5 +49,4 @@ def get_bot_response():
     return str(botresponse)
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run(debug = True)
